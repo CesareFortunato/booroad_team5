a40dd04128af4065411e566299d0535fb2e44d33
Giuseppe Mele <giuseppemele9410@gmail.com>
2026-02-27 18:00:05 +0100

    aggiunta logo in mainHeader

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -2,10 +2,14 @@ import { Link } from "react-router-dom"
 
 function MainHeader() {
     return (
-        <header className="bg-white border-bottom shadow-sm">
+        <header className="bg-white border-bottom shadow-sm sticky-top">
             <div className="container py-3 d-flex justify-content-center">
                 <Link to="/" className="text-decoration-none">
-                    <span className="fs-2 fw-bold text-primary">BooRoad</span>
+                    <img
+                        src="/images/booRoad_logo (2).png"
+                        alt="BooRoad"
+                        style={{ height: "60px", width: "auto" }}
+                    />
                 </Link>
             </div>
         </header>
