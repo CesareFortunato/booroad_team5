fe2cc5a792e7793696399fc6d0ba38cc5c33177b
Giuseppe Mele <giuseppemele9410@gmail.com>
2026-02-27 18:09:23 +0100

    fix header

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 function MainHeader() {
     return (
-        <header className="bg-white border-bottom shadow-sm sticky-top">
+        <header className=" bg-white  border-bottom shadow-sm sticky-top " >
             <div className="container py-3 d-flex justify-content-center">
                 <Link to="/" className="text-decoration-none">
                     <img
