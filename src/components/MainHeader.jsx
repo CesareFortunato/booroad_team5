6ef19478e6d04316d66f5718f2c20043c3dd885b
Clara Loforti <clara.loforti@gmail.com>
2026-02-27 17:27:26 +0100

    style momentaneo header

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -2,8 +2,9 @@ import { Link } from "react-router-dom"
 
 function MainHeader() {
     return (
-        <header>
-            <Link to="/">
+        <header
+            className="p-4 bg-black">
+            <Link to="/" className="text-decoration-none text-white">
                 <h1>BooRoad</h1>
             </Link>
         </header>
